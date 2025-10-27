1ff1ac3a4dd0f6b9c9249c8db25d57cf362e8815
WhiteJbb <gunni6112@gmail.com>
2025-10-28 01:48:46 +0900

    롤백

--- 002 Code/frontend/vite.config.js
+++ 002 Code/frontend/vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    host: '0.0.0.0', // 외부 접속 허용
+    allowedHosts: [
+      'ec2-43-203-136-37.ap-northeast-2.compute.amazonaws.com', // 허용할 호스트 추가
+      'localhost',
+      '127.0.0.1'
+    ],
     // 개발 중 캐싱 방지
     headers: {
       'Cache-Control': 'no-store'
